c88baf7290a71e7b5b5e9376dc501d109ba47b9e
Harith11 <90240994+Harith11@users.noreply.github.com>
2024-12-27 09:51:07 +0800

    upload

---
+++ scripts/download_datasets.py
@@ -0,0 +1,236 @@
+""" This script downloads datasets that are easily retrievable and places them in the data/landing directory """
+
+
+# import necessary libraries and functions
+import os
+import zipfile
+import urllib.request
+
+
+output_dir = "../data/landing"
+
+# Make sure the directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+# SECTION: download tourism
+
+URL = "https://www.tra.gov.au/content/dam/austrade-assets/global/wip/tra/documents/tsa/2023/tra-stsa-state-tourism-satellite-accounts-2022-23-data-tables.xlsx"
+
+urllib.request.urlretrieve(URL, f"{output_dir}/tourism.xlsx")
+
+print("tourism data downloaded")
+
+# Coastline data
+url = "https://d28rz98at9flks.cloudfront.net/61395/61395_shp.zip"
+# output directories
+local_zip_path = f"{output_dir}/coastline.zip"
+local_output_dir = f"{output_dir}/coastline"
+
+# retrieve the coastline data
+urllib.request.urlretrieve(url, local_zip_path)
+with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+    zip_ref.extractall(local_output_dir)
+
+# delete original zip file
+os.remove(local_zip_path)
+
+print("Coastline Data Download Complete")
+
+
+
+# SECTION: download the shp file of SA2 borders in Australia
+
+# Base URL for ABS statistics
+BASE_URL = "https://www.abs.gov.au/statistics/standards/"
+# Path to the ASGS edition and time period
+ASGS_VERSION = "australian-statistical-geography-standard-asgs-edition-3/"
+TIME_PERIOD = "jul2021-jun2026/"
+# Path to the download files and the specific file name
+DOWNLOAD_PATH = "access-and-downloads/digital-boundary-files/"
+FILE_NAME = "SA2_2021_AUST_SHP_GDA2020.zip"
+
+# Complete URL for the SA2 borders digital boundary file
+SA2_BORDERS_LINK = BASE_URL + ASGS_VERSION + TIME_PERIOD + DOWNLOAD_PATH + FILE_NAME
+
+# output directories
+local_zip_path = f"{output_dir}/{FILE_NAME}"
+local_output_dir = f"{output_dir}/SA2_2021_map"
+
+# download the zip file for the SA2 borders and extract the contents of the zip file
+urllib.request.urlretrieve(SA2_BORDERS_LINK, local_zip_path) 
+with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+   zip_ref.extractall(local_output_dir)
+
+# delete orginal zip file  
+os.remove(local_zip_path)
+
+print("SA2 Borders Download Complete")
+
+
+
+# SECTION: download the historical rental data
+
+# output directories
+local_output_dir = f"{output_dir}/moving-annual-rent-suburb-march-quarter-2024.xlsx"
+
+# URL for historical rental data
+BASE_URL = "https://www.dffh.vic.gov.au/"
+FILE_NAME = "moving-annual-rent-suburb-march-quarter-2024-excel"
+HISTORICAL_RENTAL_LINK = BASE_URL + FILE_NAME
+
+# retrieve historical rental data
+urllib.request.urlretrieve(HISTORICAL_RENTAL_LINK, local_output_dir) 
+
+print("Historical Rental Data Download Complete")
+
+
+
+# SECTION: download the ABS data
+years = [(2016,'SSC') ,(2021, 'SAL'), (2021, 'SA2'), (2016, 'SA2')]
+BASE_URL = "https://www.abs.gov.au/census/find-census-data/datapacks/download/"
+
+
+for year, tp in years:
+    # URL for the ABS data
+    FILE_NAME = f"{year}_GCP_{tp}_for_VIC_short-header.zip"
+    ABS_DATA_LINK = BASE_URL + FILE_NAME
+
+    # output directories
+    local_zip_path = f"{output_dir}/{FILE_NAME}"
+    local_output_dir = f"{output_dir}/{year}_GCP_{tp}_for_VIC_short-header"
+
+    # retrieve the ABS data
+    urllib.request.urlretrieve(ABS_DATA_LINK, local_zip_path) 
+    with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(local_output_dir)
+
+    # delete orginal zip file  
+    os.remove(local_zip_path)
+
+
+
+# SECTION: download the LGA correspondences file
+FILE_NAME = "CG_SSC_2016_SAL_2021.csv"
+local_correspondence_output_path = f"{output_dir}/{FILE_NAME}"
+CORRESPONDENCE_LINK = f'https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/correspondences/{FILE_NAME}'
+
+# retrieve the ABS data
+urllib.request.urlretrieve(CORRESPONDENCE_LINK, local_correspondence_output_path)
+
+print("ABS Data Download Complete")
+
+
+# SECTION: download the train station data - (METROPOLITIAN STATIONS)
+
+# output directories
+local_output_dir = f"{output_dir}/Annual_Metropolitan_Train_Station_Entries_2023-24.csv"
+
+# URL for train station data
+BASE_URL = "https://vicroadsopendatastorehouse.vicroads.vic.gov.au/"
+DOWNLOAD_PATH = "opendata/Public_Transport/Patronage/Annual_metropolitan_train_station_entries/"
+FILE_NAME = "Annual_Metropolitan_Train_Station_Entries_2023-24.csv"
+TRAIN_LINK = BASE_URL + DOWNLOAD_PATH + FILE_NAME
+
+# retrieve train station data
+headers = {'User-Agent': 'Mozilla/5.0'}
+req = urllib.request.Request(TRAIN_LINK, headers=headers)
+with urllib.request.urlopen(req) as response:
+    with open(local_output_dir, 'wb') as f:
+        f.write(response.read())
+
+print("Train Stations in VIC Data Download Complete")
+
+# SECTION: download Crime records in Victoria data
+
+# output directory
+local_output_dir = f"{output_dir}/LGA_Criminal_Incidents_Year_Ending_March.csv"
+
+# URL for crime data
+BASE_URL = "https://files.crimestatistics.vic.gov.au/"
+DOWNLOAD_PATH = "2024-06/"
+FILE_NAME = "Data_Tables_LGA_Criminal_Incidents_Year_Ending_March_2024.xlsx"
+CRIME_RECORDS_LINK = BASE_URL + DOWNLOAD_PATH + FILE_NAME
+
+# retrieve historical rental data
+urllib.request.urlretrieve(CRIME_RECORDS_LINK, local_output_dir) 
+
+print("Crime Data Download Complete")
+
+
+
+# SECTION: download Open Space data
+BASE_URL = "https://opendata.arcgis.com/datasets/"
+FILE_NAME = "da1c06e3ab6948fcb56de4bb3c722449_0.zip"
+
+
+DATA_LINK = BASE_URL + FILE_NAME
+
+# output directories
+local_zip_path = f"{output_dir}/{FILE_NAME}"
+local_output_dir = f"{output_dir}/Open_Space"
+
+# retrieve the ABS data
+urllib.request.urlretrieve(DATA_LINK, local_zip_path) 
+with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+   zip_ref.extractall(local_output_dir)
+
+# delete orginal zip file  
+os.remove(local_zip_path)
+
+print("Open Space Data Download Complete")
+
+
+
+# SECTION: Download Suburbs and Locactions (SAL) data 
+
+# download SAL data
+BASE_URL = "https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files/"
+FILE_NAME = "SAL_2021_AUST_GDA2020_SHP.zip"
+
+
+DATA_LINK = BASE_URL + FILE_NAME
+
+# output directories
+local_zip_path = f"{output_dir}/{FILE_NAME}"
+local_output_dir = f"{output_dir}/SAL_data"
+
+# retrieve the ABS data
+urllib.request.urlretrieve(DATA_LINK, local_zip_path) 
+with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+   zip_ref.extractall(local_output_dir)
+
+# delete orginal zip file  
+os.remove(local_zip_path)
+
+# SECTION: download projections
+
+local_output_dir = f"{output_dir}/Population_Projections"
+
+base_urls = ["https://www.planning.vic.gov.au/__data/assets/excel_doc/0028/691660/VIF2023_SA2_Pop_Hhold_Dwelling_Projections_to_2036_Release_2.xlsx"]
+
+for url in base_urls:
+    # output directories
+    local_output_dir = f"{output_dir}/{url.split('/')[-1]}"
+
+    # retrieve the ABS data
+    urllib.request.urlretrieve(url, local_output_dir)
+
+print("Population Projections Download Complete")
+
+
+
+# SECTION: Download business listing data
+
+urllib.request.urlretrieve("https://data.melbourne.vic.gov.au/api/v2/catalog/datasets/business-establishments-with-address-and-industry-classification/exports/csv?delimiter=%2C", f"{output_dir}/business_listing.csv")
+
+print("business listing data downloaded")
+
+
+# SECTION: Download the ANZSCIC-4 data
+
+urllib.request.urlretrieve("https://www.abs.gov.au/statistics/classifications/australian-and-new-zealand-standard-industrial-classification-anzsic/2006-revision-2-0/numbering-system-and-titles#:~:text=Download-,Download,-table%20as%20CSV", f"{output_dir}/anzsic-groups.csv")
+
+print("ANZSCIC-4 data downloaded")
+
+
+
---
+++ scripts/openrouteservice_data_download.py
@@ -0,0 +1,151 @@
+import requests
+import json
+import time
+import pandas as pd
+import os
+
+# In this script ors is an acronym for openstreetmap
+
+def api_call(BASE_API_LINK, headers, suburb, payload_data, CBD):
+
+    """
+        This function performs the actual API call to ORS and retrieves the information specified in the function
+        In then returns a dictionary containing this information.
+    """
+
+    # API call limit is 40 per minute, here we make sure we do not breach that adhearing to the ORS's API policies
+    call_interval = 60 / 39 # 39 is used as a boundary (make sure we never do 40 api calls in a minute)
+    time.sleep(call_interval)
+    response = requests.post(BASE_API_LINK, headers=headers, json=payload_data)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        print(f"Success: {response.status_code}")
+        # Process and print the response data
+        data = response.json()
+
+        distance = data["routes"][0]["summary"]["distance"] # km
+        duration = data["routes"][0]["summary"]["duration"] # secs
+
+        # create a dictionary of results of time and distances
+        if CBD:
+            distance_CBD_record = {
+                "suburb_centre_latitude": payload_data["coordinates"][0][1],
+                "suburb_centre_longitude": payload_data["coordinates"][0][0],
+                "distance_to_CBD": distance,
+                "time_to_CBD": duration,
+            }
+        else: 
+            distance_CBD_record = {
+                "suburb_centre_latitude": payload_data["coordinates"][0][1],
+                "suburb_centre_longitude": payload_data["coordinates"][0][0],
+                "distance_to_station": distance,
+                "time_to_station": duration,
+            }
+        return distance_CBD_record
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)  # Print error message for debugging
+        return None
+
+
+def download_ors_proximity_data(suburb_and_coords_df, api_key, file_name):
+    """ 
+        Donwloads all distance and time by car from the centre of the suburbs in the given dataframe to the CBD.
+        This is done through an API and the distances and time is outputted into a JSON file.
+    """
+    CBD_lng = 144.9623
+    CBD_lat = -37.8124
+    proximity_to_CBD = {}
+
+    # Define the API endpoint and API key
+    ORS_BASE_URL = "https://api.openrouteservice.org"
+    DIRECTIONS_SERVICE = "/v2/directions/driving-car"
+    BASE_API_LINK = ORS_BASE_URL + DIRECTIONS_SERVICE
+    API_KEY = api_key
+
+    # Set the headers
+    headers = {
+        "Authorization": API_KEY,
+        "Content-Type": "application/json"
+    }
+
+    # exception handling of coordinates where suburb centre is too far away from a road
+    exception_coords = {
+        "Truganina": [144.69748, -37.79924],
+        "Mambourin": [144.59416, -37.89662],
+        "Cocoroc": [144.63164, -37.92867],
+        "Whittlesea": [145.11119, -37.46952],
+        "Hmas Cerberus": [145.17923, -38.38106]
+    }
+
+    api_call_count = 0
+
+    # loop through all suburbs and get distance and time from CBD
+    for suburb_index in list(suburb_and_coords_df.index):
+    
+        # retrieve suburb: name, longitude and latitude coordinates
+        suburb = suburb_and_coords_df.iloc[suburb_index]["SAL suburb"]
+        if suburb in exception_coords.keys():
+            start_lng = exception_coords[suburb][0]
+            start_lat = exception_coords[suburb][1]
+        else:
+            start_lng = suburb_and_coords_df.loc[suburb_index, "suburb_centroid"].x
+            start_lat = suburb_and_coords_df.loc[suburb_index, "suburb_centroid"].y
+        
+        # Define the request payload
+        payload_data = {
+            "coordinates": [
+                [start_lng, start_lat],  # Note the order: [longitude, latitude]
+                [CBD_lng, CBD_lat]
+            ]
+        }
+        api_call_count += 1
+
+        # Make the POST request
+        distance_CBD_record = api_call(BASE_API_LINK, headers, suburb, payload_data, True)
+        if distance_CBD_record != None:
+            # Define the request payload for station 1
+            payload_data = {
+                "coordinates": [
+                    [suburb_and_coords_df.loc[suburb_index, "closest_station_1_LONGITUDE"], 
+                    suburb_and_coords_df.loc[suburb_index, "closest_station_1_LATITUDE"]], 
+                    [start_lng, start_lat]
+                ]
+            }
+            distance_train1_record = api_call(BASE_API_LINK, headers, suburb, payload_data, False)
+
+            # Define the request payload for station 2
+            payload_data = {
+                "coordinates": [
+                    [suburb_and_coords_df.loc[suburb_index, "closest_station_2_LONGITUDE"], 
+                    suburb_and_coords_df.loc[suburb_index, "closest_station_2_LATITUDE"]], 
+                    [start_lng, start_lat]
+                ]
+            }
+            distance_train2_record = api_call(BASE_API_LINK, headers, suburb, payload_data, False)
+            
+            # keep data on the closest station only (distance and time to closest station is the only relevant data here)
+            if distance_train1_record["distance_to_station"] < distance_train2_record["distance_to_station"]:
+                closest_station_record = distance_train1_record
+            else:
+                closest_station_record = distance_train2_record
+            
+            distance_CBD_record["distance_to_station"] = closest_station_record["distance_to_station"]
+            distance_CBD_record["time_to_station"] = closest_station_record["time_to_station"]
+            proximity_to_CBD[suburb] = distance_CBD_record
+        else:
+            proximity_to_CBD[suburb] = {
+
+            }
+        print(f"suburb(s) no. {api_call_count} complete")
+
+    # Output File path
+    file_path = f"../../data/landing/{file_name}.json"
+
+    data = proximity_to_CBD
+
+    # Write the updated data back to the file
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+    return proximity_to_CBD
